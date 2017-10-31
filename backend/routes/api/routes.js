d12e30ccdb0e6d8a5ad18190dcc827c3c8c0fcfb
Aleksei Abramov <lexa0303@ukr.net>
2017-10-31 10:37:31 +0200

    user repository and routes created

--- backend/routes/api/routes.js
+++ backend/routes/api/routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
 	return {
+		userRoutes: require('./userRoutes')(app),
 	};
 };
---
+++ backend/routes/api/userRoutes.js
@@ -0,0 +1,58 @@
+const
+	apiResponse = require('express-api-response'),
+	userRepository = require('../../repositories/userRepository'),
+	baseUrl = '/api/user/';
+
+module.exports = function (app) {
+	app.get(baseUrl + 'me', function (req, res, next) {
+		userRepository.getById(req.session.passport.user)
+			.then(data => {
+				res.data = data;
+				next();
+			})
+			.catch(err => {
+				res.err = err;
+				next();
+			})
+	}, apiResponse);
+
+	app.get(baseUrl + ':id', function (req, res, next) {
+		userRepository.getById(req.params.id)
+			.then((data) => {
+				res.data = data;
+				next();
+			})
+			.catch((err) => {
+				res.err = err;
+				next();
+			})
+	}, apiResponse);
+
+	app.post(baseUrl, function (req, res, next) {
+		next();
+	}, apiResponse);
+
+	app.put(baseUrl + ':id', function (req, res, next) {
+		userService.updateItem(req.params.id, req.body)
+			.then(data => {
+				res.data = data;
+				next();
+			})
+			.catch(err => {
+				res.err = err;
+				next();
+			})
+	}, apiResponse);
+
+	app.delete(baseUrl + ':id', function (req, res, next) {
+		userRepository.deleteById(req.params.id)
+			.then(data => {
+				res.data = data;
+				next();
+			})
+			.catch(err => {
+				res.err = err;
+				next();
+			})
+	}, apiResponse);
+};
